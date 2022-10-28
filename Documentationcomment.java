00b1d08d4f5b500797f66315b62f217fea99f7e7
widathkhan <114463526+widathkhan@users.noreply.github.com>
2022-10-28 15:22:18 +0530

    Create Documentationcomment.java

---
+++ Documentationcomment.java
@@ -0,0 +1,20 @@
+package Practicecodes.Java.Closedbook;
+/**Documentation comment it is
+*/
+public class Documentationcomment{
+ /*Documentationcomment.java:3: error: class Documentation is public, should be declared in a file named Documentation.java
+  For attempting with different file names and different class name*/
+  public static void main(){
+/*Error: Main method not found in class Documentationcomment, please define the main method as:
+   public static void main(String[] args)
+   For attempting to run without public */
+
+/*Error: Main method is not static in class Documentationcomment, please define the main method as:
+   public static void main(String[] args)
+  For attempting to run without static */  
+
+/*Error: Main method not found in class Documentationcomment, please define the main method as:
+   public static void main(String[] args)
+  For attempting to run without parameters*/
+  }
+}
