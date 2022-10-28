32bccd60eb414cb7a14907278e4f350ecfa80d02
widathkhan <114463526+widathkhan@users.noreply.github.com>
2022-10-28 15:21:45 +0530

    Create DefaultDatatypes.java

---
+++ DefaultDatatypes.java
@@ -0,0 +1,23 @@
+package Practicecodes.Java.Closedbook;
+
+public class DefaultDatatypes {
+    byte b;
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
+    char c;
+    boolean bool;
+    public static void main(String[] args) {
+        DefaultDatatypes obj=new DefaultDatatypes();
+        System.out.println("Byte "+obj.b);
+        System.out.println("Short "+obj.s);
+        System.out.println("Int "+obj.i);
+        System.out.println("Long "+obj.l);
+        System.out.println("Float "+obj.f);
+        System.out.println("Double "+obj.d);
+        System.out.println("Char "+obj.c);
+        System.out.println("Boolean "+obj.bool);
+    }
+}
