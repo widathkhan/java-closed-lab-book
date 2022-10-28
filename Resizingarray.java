055e1ca1b39c7a9dbeb5153289cf6780435c7862
widathkhan <114463526+widathkhan@users.noreply.github.com>
2022-10-28 15:32:52 +0530

    Create Resizingarray.java

---
+++ Resizingarray.java
@@ -0,0 +1,7 @@
+package Practicecodes.Java.Closedbook;
+
+public class Resizingarray {
+    public static void main(String[] args) {
+        System.out.println("We cannot resize array in java");
+    }
+}
